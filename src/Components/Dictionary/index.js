993c72e1013983ce5835621524194b956891c9d4
Mell <mellkayrusson@gmail.com>
2020-05-22 11:32:29 +0100

    search bar added

--- src/Components/Dictionary/index.js
+++ src/Components/Dictionary/index.js
@@ -7,8 +7,13 @@ const dummyData = [
   },
   {
     name: "Baby Function",
-    descriptions:
-      "Created by Vini, this is an alternative name for fat arrow functions.",
+    description:
+      "Created by Vini and then shamelessly stolen by Ben, this is an alternative name for fat arrow functions.",
+  },
+  {
+    name: "Binny Pong Quizzy Song",
+    description:
+      "One of the many energizers Ben came up with; It involves throwing a ball into a bin, and if it lands in Ben will sing you a question and you must get both the answer and the song correct for points.",
   },
 ];
 
@@ -16,8 +21,15 @@ function Dictionary() {
   return (
     <div className="wrapper">
       <h2 className="tabTitle"> Dictionary </h2>
+      <input placeholder="Search For a Benism..." className="searchBar" />
       {dummyData.map((item) => {
-        return <p> {item.name} </p>;
+        return (
+          <>
+            <p style={{ fontWeight: "bold" }}> {item.name}: </p>
+            <p> {item.description}</p>
+            <hr style={{ width: "90%" }} />
+          </>
+        );
       })}
     </div>
   );
