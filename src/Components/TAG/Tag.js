398a05290b9e4c9a4cb9b2b029c6f5f626bad94c
Mell <mellkayrusson@gmail.com>
2020-05-22 15:02:31 +0100

    tag stuff done

--- src/Components/TAG/Tag.js
+++ src/Components/TAG/Tag.js
@@ -1,33 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "../Form/index.css";
+import "./tag.css";
 
 const livingThings = [
-  'bee',
-  'cat',
-  'Ben',
-  'Pacman',
-  'ovenbird',
-  'pigeon',
-  'dog',
-  'clone',
-  'ancient mega-sloth',
-  'Patrikachu',
+  "bee",
+  "cat",
+  "Ben",
+  "Pacman",
+  "ovenbird",
+  "pigeon",
+  "dog",
+  "clone",
+  "ancient mega-sloth",
+  "Patrikachu",
 ];
 
 const objects = [
-  'a rubber pancake',
-  'a hose',
-  'ears',
-  'a fake plant',
-  'a Pokeball',
-  'a beet-route',
-  'teppanyaki',
-  'jazz',
-  'a nice biscuit',
-  'cwissyAI',
+  "a rubber pancake",
+  "a hose",
+  "ears",
+  "a fake plant",
+  "a Pokeball",
+  "a beet-route",
+  "teppanyaki",
+  "jazz",
+  "a nice biscuit",
+  "cwissyAI",
 ];
 
 function Tag() {
-  const [tagInput, setTagInput] = useState('');
+  const [tagInput, setTagInput] = useState("");
   const [showTag, setShowTag] = useState(false);
 
   function handleChange(e) {
@@ -38,19 +40,36 @@ function Tag() {
     setShowTag(true);
   }
 
+  function clearInput() {
+    setTagInput("");
+    setShowTag(false);
+  }
+
   return (
-    <div className="wrapper">
-      <h2 className="tabTitle"> Tenuous Analogy Generator </h2>
-      <div id="tagInput">
-        <input value={tagInput} onChange={handleChange} />
-        <button onClick={handleClick}>Submit</button>
+    <>
+      <div className="wrapper">
+        <h2 className="tabTitle"> Tenuous Analogy Generator </h2>
+        <div id="tagInput">
+          <input
+            value={tagInput}
+            onChange={handleChange}
+            className="formInput"
+            onClick={clearInput}
+          />
+          <button onClick={handleClick} className="addButton">
+            Submit
+          </button>
+        </div>
       </div>
+
       {showTag && (
-        <h3>{`Ben says: A ${tagInput} is like ${
-          livingThings[Math.floor(Math.random() * livingThings.length)]
-        } with ${objects[Math.floor(Math.random() * objects.length)]}`}</h3>
+        <div className="TAGWrapper">
+          <h3>{`Ben says: A ${tagInput} is like ${
+            livingThings[Math.floor(Math.random() * livingThings.length)]
+          } with ${objects[Math.floor(Math.random() * objects.length)]}`}</h3>
+        </div>
       )}
-    </div>
+    </>
   );
 }
 
